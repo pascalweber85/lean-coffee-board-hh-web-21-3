0d37f155f409c86521bea2fc24cfaa63f08e32e8
Pascal Weber <weber-pascal@web.de>
2021-06-03 11:39:47 +0200

    complete Create Card

--- client/src/components/CreateCard.js
+++ client/src/components/CreateCard.js
@@ -1,7 +1,27 @@
+import styled from 'styled-components/macro'
 
-  import styled from 'styled-components/macro'
-    
-  export default function CreateCard () {
-      return <div>CreateCard</div>
-  }
-        
+export default function CreateCard() {
+  return (
+    <Wrapper>
+      <button>&lt; back</button>
+      <form>
+        <label>
+          Text:
+          <textarea rows="3" cols="33" />
+        </label>
+
+        <label>
+          Author:
+          <input type="textarea" />
+        </label>
+
+        <button>Create Card</button>
+      </form>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.section`
+  display: grid;
+  gap: 20px;
+`
--- client/src/components/CreateCard.stories.js
+++ client/src/components/CreateCard.stories.js
@@ -1,12 +1,10 @@
+import CreateCard from './CreateCard'
+export default {
+  title: 'CreateCard',
+  component: CreateCard,
+}
 
-  import CreateCard from './CreateCard'
-  export default {
-      title: 'CreateCard',
-      component: CreateCard
-  }
-    
-  const Template = args => <CreateCard {...args} />
-    
-  export const Default = Template.bind({})
-  Default.args = {}
-        
+export const Template = args => <CreateCard {...args} />
+
+// export const Default = Template.bind({})
+// Default.args = {}
